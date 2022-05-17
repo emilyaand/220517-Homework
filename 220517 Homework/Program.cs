7b3a20a9ed1233001fe7367d89d5f2ebd89dc588
EmiliyaAndamova <EmiliyaAndamova@WPIPCM>
2022-05-17 15:27:33 +0400

    Add project files.

---
+++ 220517 Homework/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _220517_Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string str = "Nurlan Faiq Emil";
+
+            var chars = str.ToCharArray();
+            var length = chars.Length;
+
+            Console.WriteLine($"String length is: {length}");
+
+            for (var i = length-1; i >= 0; i--)
+            {
+                Console.WriteLine(chars[i]);
+            }
+
+        }
+
+
+    }
+}
